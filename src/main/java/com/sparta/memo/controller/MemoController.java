a4c6b779d0aa22a27fea2cce644bf7c08d34fb5f
ming <oterges00@gmail.com>
2025-09-15 12:24:47 +0900

    Update, Delete 구현

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -50,4 +50,34 @@ public class MemoController {
 
         return responseList;
     }
+
+    //데이터 수정 -> 데이터 필요 -> client 바디 부분에서 데이터가 넘어옴 (json 형식) -> requestBody로 받기
+    @PutMapping("/memos/{id}") //PathVariable 방식, id만 반환할 것임
+    //id : 데이터 불러오기 위해 , requestDto json형식으로 넘어온 데이터를 받기 위해
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)) { //containds 해당 키가 있는가? -> t/f
+            //해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            //memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        //해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)) {
+            //해당 메모를 삭제
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
 }
